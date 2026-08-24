e8dc17a49d64bd7dafc94eb3a0aa5192a98551be
fortezzalaboratory <fortezzalabs@gmail.com>
2026-08-24 22:18:25 +0100

    feat(reconciliation): add payroll reconciliation snapshot builder
    Resolves #256

--- packages/core/src/reconciliation/index.ts
+++ packages/core/src/reconciliation/index.ts
@@ -1,7 +1,9 @@
 export { generateReconciliationDiff } from "./ReconciliationDiffGenerator";
+export { ReconciliationSnapshotBuilder } from "./SnapshotBuilder";
 export type {
   ObservedPaymentState,
   ReconciliationDiffCategory,
   ReconciliationDiffEntry,
   ReconciliationDiffResult,
 } from "./types";
+export type { ReconciliationSnapshot, SnapshotInput, SnapshotComparison } from "./SnapshotBuilder";
